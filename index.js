35d6204ac20ce53236a7374c4d13c6471c32cae8
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-07-04 18:25:22 +0200

    add displayCart function

--- index.js
+++ index.js
@@ -1,36 +1,83 @@
 const cart = [];
 
-// const displayCart = (cart) => {
-//   const listItems = document.getElementById('cartItems');
-//   listItems.innerHTML = '';
+// When click 'Cart Icon'
+const displayCart = (cart) => {
+  const cartItems = document.getElementById('cart');
+  cartItems.innerHTML = '';
 
-//   cart.map((item) => {
-//     const oneItem = document.createElement('li');
-//     oneItem.textContent = item;
-//     listItems.appendChild(oneItem);
-//   });
-// };
+  cart.map((item, index) => {
+    const oneItemInfo = document.createElement('div');
+    const oneItemPrice = document.createElement('div');
+    const oneItemQuantity = document.createElement('div');
+    const oneItemSubtotal = document.createElement('div');
 
-const addToCart = (newItem) => {
-  let itemFound = cart.find((item) => item.id === newItem.id)
-  console.log('itemFound', itemFound);
-  
-    if (itemFound) {
-      itemFound.quatity++;
-      itemFound.subTotal += Number(newItem.price);
-    } else {
-      let quatity = 1;
-      let subTotal = +newItem.price * quatity;
-    
-      cart.push({ ...newItem, quatity: quatity, subTotal: subTotal });
-      console.log('new item added');
-    }
+    oneItemInfo.textContent = `${item.name}, ${item.category}, ${item.color}, ${item.size}`;
+    oneItemPrice.textContent = `${item.price}`;
+    oneItemQuantity.textContent = `${item.quantity}`;
+    oneItemSubtotal.textContent = `${item.subTotal}`;
 
-  // displayCart(cart);
+    cartItems
+      .appendChild(oneItemInfo)
+      .appendChild(oneItemPrice)
+      .appendChild(oneItemQuantity)
+      .appendChild(oneItemSubtotal);
+  });
 };
 
-addToCart({ id: 1, name: 'shoe', price: '19.99' });
-addToCart({ id: 2, name: 'sh', price: '19.99' });
-addToCart({ id: 1, name: 'shoe', price: '19.99' });
-console.log(cart);
-console.log(cart.keys());
+displayCart(cart);
+
+// When click 'Cart Icon'
+const sumCartItems = (array) => {
+  return cart.reduce((a, b) => a.subTotal + b.subTotal);
+};
+
+// when click 'Buy now'
+const addToCart = (newItem) => {
+  let itemFound = cart.find((item) => item.id === newItem.id);
+
+  if (itemFound) {
+    itemFound.quantity++;
+    itemFound.subTotal += Number(newItem.price);
+  } else {
+    let quantity = 1;
+    let subTotal = +newItem.price * quantity;
+    cart.push({ ...newItem, quantity: quantity, subTotal: subTotal });
+  }
+  console.log(cart);
+  displayCart(cart);
+};
+
+addToCart({
+  id: 1,
+  name: 'shoe',
+  category: 'Mens TShirt',
+  color: 'black',
+  size: 'M',
+  price: '19.99',
+});
+addToCart({
+  id: 2,
+  name: 'sho',
+  category: 'Women TShirt',
+  color: 'blue',
+  size: 'M',
+  price: '19.99',
+});
+addToCart({
+  id: 1,
+  name: 'shoe',
+  category: 'Mens TShirt',
+  color: 'black',
+  size: 'M',
+  price: '19.99',
+});
+addToCart({
+  id: 3,
+  name: 'tops',
+  category: 'Women TShirt',
+  color: 'black',
+  size: 'M',
+  price: '19.99',
+});
+
+sumCartItems(cart);
